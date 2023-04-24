b93c09b74629a7a936b16ba09d039ea1051fb258
trinhhoacai <114391320+trinhhoacai@users.noreply.github.com>
2023-04-24 14:47:43 +0700

    Add files via upload

---
+++ chanle.java
@@ -0,0 +1,20 @@
+public class chanle {
+    //sử dụng vòng lặp for để in ra số chẵn từ 1-10
+    public static void main(String[] args) {
+        int so=10;
+        for(int i = 1;i<=so;i++){//dung vong lap for de khai bao tu 1-10
+            if(i%2==0){
+                System.out.println(i);//su dung cau truc dieu kien in ra cac so chan thoa man dieu kien i%2==0;
+            }
+        }
+    }
+    }
+
+
+
+
+
+
+
+
+
